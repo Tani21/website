408ebd162622e804130e9d6141393079443c03d5
Tani21 <tanishkavaswani1@gmail.com>
2023-11-08 22:45:39 +0530

    alt text changed

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <div>
       <div className="flex items-center justify-center w-screen h-screen bg-blue-900">
         <div className="flex flex-row justify-center items-center">
-          <img src="./logo.png" width={60} alt="Logo" />
+          <img src="./logo.png" width={60} alt="Website Logo" />
           <img src="./name.png" width={250} alt="Name" />
         </div>
       </div>
