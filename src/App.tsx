6967291f00da610e61f7729f2b129d0c9fb09962
Tani21 <tanishkavaswani1@gmail.com>
2023-11-08 21:51:19 +0530

    bg colour changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
 
   return (
     <div>
-      <div className="flex items-center justify-center w-screen h-screen">
+      <div className="flex items-center justify-center w-screen h-screen bg-blue-900">
         <div className="flex flex-row justify-center items-center">
           <img src="./logo.png" width={60} alt="Logo" />
           <img src="./name.png" width={250} alt="Name" />
